8e00438b948b295193d516ad59efb1c2fe5159a7
Adrien Pauchet <grandchefdotkong@gmail.com>
2023-03-05 20:52:38 +0900

    update

--- minesweeper/main.cpp
+++ minesweeper/main.cpp
@@ -1,34 +1,37 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <time.h>
 
 using namespace sf;
 
-const unsigned int WINDOW_WIDTH(14);
-const unsigned int WINDOW_HEIGHT(14);
-const unsigned int NUMBER_MINES(18);
-const unsigned int SPRITE_SIZE(32);
-const unsigned int MINES(9);
-const unsigned int EMPTY(0);
+const unsigned short int WINDOW_WIDTH(14);
+const unsigned short int WINDOW_HEIGHT(14);
+const unsigned short int NUMBER_MINES(18);
+const unsigned short int SPRITE_SIZE(32);
+const unsigned short int MINES(9);
+const unsigned short int EMPTY(0);
 
 enum GAME { WIN, LOOSE, CONTINUE };
 
-
-void generateMinesField(int solutionGrid[][WINDOW_HEIGHT]);
-int handleMouseClick(int solutionGrid[][WINDOW_HEIGHT], bool clickGrid[][WINDOW_HEIGHT], int x, int y);
+void generateMinesField(unsigned short int solutionGrid[][WINDOW_HEIGHT]);
+GAME handleMouseClick(unsigned short int solutionGrid[][WINDOW_HEIGHT], 
+  bool clickGrid[][WINDOW_HEIGHT], int x, int y, unsigned short int &numberOfClick);
 void handleGameOver(bool clickGrid[][WINDOW_HEIGHT]);
-
+GAME checkVictory(unsigned short int numberOfClick);
 
 int main() {
 
   srand(time(0));
 
-  RenderWindow app(VideoMode(WINDOW_WIDTH*32, WINDOW_HEIGHT*32), "Minesweeper!");
+  RenderWindow app(VideoMode(WINDOW_WIDTH*SPRITE_SIZE, WINDOW_HEIGHT*SPRITE_SIZE), 
+    "Minesweeper!");
   app.setFramerateLimit(60);
 
-  int solutionGrid[WINDOW_WIDTH][WINDOW_HEIGHT] = {};
+  unsigned short int solutionGrid[WINDOW_WIDTH][WINDOW_HEIGHT] = {};
   bool clickGrid[WINDOW_WIDTH][WINDOW_HEIGHT] = {}; 
 
-  int gameStatus = CONTINUE;
+  unsigned short int gameStatus = CONTINUE;
+  unsigned short int numberOfClick(0);
 
   Texture texture;
   texture.loadFromFile("images/tiles.jpg");
@@ -36,13 +39,11 @@ int main() {
 
   generateMinesField(solutionGrid);
 
-
-
   while (app.isOpen() && gameStatus == CONTINUE) {
 
-    Vector2i pos = Mouse::getPosition(app);
-    int x = pos.x / SPRITE_SIZE;
-    int y = pos.y / SPRITE_SIZE;
+    Vector2i mousePosition = Mouse::getPosition(app);
+    int x = mousePosition.x / SPRITE_SIZE;
+    int y = mousePosition.y / SPRITE_SIZE;
 
     Event e;
     while (app.pollEvent(e)) {
@@ -53,15 +54,17 @@ int main() {
 
       if (e.type == Event::MouseButtonPressed) {
         if (e.key.code == Mouse::Left) { 
-          gameStatus = handleMouseClick(solutionGrid, clickGrid, x, y);
+          gameStatus = handleMouseClick(solutionGrid, clickGrid, x, y, numberOfClick);
         }
       }
     }
 
+    gameStatus = checkVictory(numberOfClick);
+
     app.clear(Color::White);
 
-    for (int i(0); i < WINDOW_WIDTH; ++i) {
-      for (int j(0); j < WINDOW_HEIGHT; ++j) {
+    for (short int i(0); i < WINDOW_WIDTH; ++i) {
+      for (short int j(0); j < WINDOW_HEIGHT; ++j) {
           
         if(clickGrid[i][j]) {
           sprite.setTextureRect(IntRect(solutionGrid[i][j] * SPRITE_SIZE, 0, SPRITE_SIZE, SPRITE_SIZE));
@@ -81,14 +84,14 @@ int main() {
 }
 
 
-void generateMinesField(int solutionGrid[][WINDOW_HEIGHT]) {
-  for(int mines(0); mines < NUMBER_MINES; ++mines) {
-    const unsigned int numberOfCases(WINDOW_WIDTH*WINDOW_HEIGHT);
+void generateMinesField(unsigned short int solutionGrid[][WINDOW_HEIGHT]) {
+  for(short int mines(0); mines < NUMBER_MINES; ++mines) {
+    const unsigned short int numberOfCases(WINDOW_WIDTH*WINDOW_HEIGHT);
 
-    const int minesPosition = rand() % numberOfCases;
+    const short int minesPosition = rand() % numberOfCases;
 
-    int i = minesPosition / WINDOW_WIDTH;
-    int j = minesPosition % WINDOW_HEIGHT;
+    unsigned short int i = minesPosition / WINDOW_WIDTH;
+    unsigned short int j = minesPosition % WINDOW_HEIGHT;
 
     if(solutionGrid[i][j] == MINES) {
       --mines;
@@ -109,88 +112,71 @@ void generateMinesField(int solutionGrid[][WINDOW_HEIGHT]) {
 }
 
 
-int handleMouseClick(int solutionGrid[][WINDOW_HEIGHT], bool clickGrid[][WINDOW_HEIGHT], int x, int y) {
+GAME handleMouseClick(unsigned short int solutionGrid[][WINDOW_HEIGHT], 
+  bool clickGrid[][WINDOW_HEIGHT], int x, int y, unsigned short int &numberOfClick) {
   
   clickGrid[x][y] = true;
+  ++numberOfClick;
 
   if(solutionGrid[x][y] == MINES) {
     handleGameOver(clickGrid);
     return LOOSE;
   }
 
-  if(solutionGrid[x][y] == EMPTY) {
-    if(x < (WINDOW_WIDTH-1)) {
-      if(solutionGrid[x+1][y] == EMPTY && !clickGrid[x+1][y]) {
-        handleMouseClick(solutionGrid, clickGrid, x+1, y);
-      } else {
-        clickGrid[x+1][y] = true;
-      }
-    }
+  if(solutionGrid[x][y] != EMPTY) {
+    return CONTINUE;
+  }
 
-    if(y < (WINDOW_HEIGHT-1)) { 
-      if(solutionGrid[x][y+1] == EMPTY && !clickGrid[x][y+1]) {
-        handleMouseClick(solutionGrid, clickGrid, x, y+1);
-      } else {
-        clickGrid[x][y+1] = true;
-      }
-    }
+  if(x < (WINDOW_WIDTH-1) && !clickGrid[x+1][y]) {
+    handleMouseClick(solutionGrid, clickGrid, x+1, y, numberOfClick);
+  }
 
-    if(x > 0) {
-      if(solutionGrid[x-1][y] == EMPTY && !clickGrid[x-1][y]) {
-        handleMouseClick(solutionGrid, clickGrid, x-1, y);
-      } else {
-        clickGrid[x-1][y] = true;
-      }
-    }
+  if(y < (WINDOW_HEIGHT-1) && !clickGrid[x][y+1]) {
+    handleMouseClick(solutionGrid, clickGrid, x, y+1, numberOfClick);
+  }
 
-    if(y > 0) {
-      if(solutionGrid[x][y-1] == EMPTY && !clickGrid[x][y-1]) {
-        handleMouseClick(solutionGrid, clickGrid, x, y-1);
-      } else {
-        clickGrid[x][y-1] = true;
-      }
-    }
+  if(x > 0 && !clickGrid[x-1][y]) {
+    handleMouseClick(solutionGrid, clickGrid, x-1, y, numberOfClick);
+  }
 
-    if(x < (WINDOW_WIDTH-1) && y < (WINDOW_HEIGHT-1)) {
-      if(solutionGrid[x+1][y+1] == EMPTY && !clickGrid[x+1][y+1]) {
-        handleMouseClick(solutionGrid, clickGrid, x+1, y+1);
-      } else {
-        clickGrid[x+1][y+1] = true;
-      }
-    }
+  if(y > 0 && !clickGrid[x][y-1]) {
+    handleMouseClick(solutionGrid, clickGrid, x, y-1, numberOfClick);
+  }
 
-    if(x > 0 && y > 0) {
-      if(solutionGrid[x-1][y-1] == EMPTY && !clickGrid[x-1][y-1]) {
-        handleMouseClick(solutionGrid, clickGrid, x-1, y-1);
-      } else {
-        clickGrid[x-1][y-1] = true;
-      }
-    }
+  if(x < (WINDOW_WIDTH-1) && y < (WINDOW_HEIGHT-1) && !clickGrid[x+1][y+1]) {
+    handleMouseClick(solutionGrid, clickGrid, x+1, y+1, numberOfClick);
+  }
 
-    if(x > 0 && y < (WINDOW_HEIGHT-1)) { 
-      if(solutionGrid[x-1][y+1] == EMPTY && !clickGrid[x-1][y+1]) {
-        handleMouseClick(solutionGrid, clickGrid, x-1, y+1);
-      } else {
-        clickGrid[x-1][y+1] = true;
-      }
-    }
+  if(x > 0 && y > 0 && !clickGrid[x-1][y-1]) {
+    handleMouseClick(solutionGrid, clickGrid, x-1, y-1, numberOfClick);
+  }
 
-    if(x < (WINDOW_WIDTH-1) && y > 0) {
-      if(solutionGrid[x+1][y-1] == EMPTY && !clickGrid[x+1][y-1]) {
-        handleMouseClick(solutionGrid, clickGrid, x+1, y-1);
-      } else {
-        clickGrid[x+1][y-1] = true;
-      }
-    }
+  if(x > 0 && y < (WINDOW_HEIGHT-1) && !clickGrid[x-1][y+1]) {
+    handleMouseClick(solutionGrid, clickGrid, x-1, y+1, numberOfClick);
+  }
+
+  if(x < (WINDOW_WIDTH-1) && y > 0 && !clickGrid[x+1][y-1]) {
+    handleMouseClick(solutionGrid, clickGrid, x+1, y-1, numberOfClick);
   }
 
   return CONTINUE;
 } 
 
 void handleGameOver(bool clickGrid[][WINDOW_HEIGHT]) {
-  for(int i(0); i < WINDOW_WIDTH; ++i) {
-    for(int j(0); j < WINDOW_HEIGHT; ++j) {
+  for(short int i(0); i < WINDOW_WIDTH; ++i) {
+    for(short int j(0); j < WINDOW_HEIGHT; ++j) {
       clickGrid[i][j] = true;
     }
   }
+
+  std::cout << "You Lose !!" << std::endl;
+}
+
+GAME checkVictory(unsigned short int numberOfClick) {
+  if(numberOfClick == (WINDOW_WIDTH * WINDOW_HEIGHT - NUMBER_MINES)) {
+    std::cout << "You Win !!" << std::endl;
+    return WIN;
+  }
+
+  return CONTINUE;
 }
